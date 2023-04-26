97e48aa0e95c453be9c489798253961f87c81800
Metalcry1 <metalcry1@gmail.com>
2023-04-26 11:36:45 +0200

    Agrego rendondeo a las cantidades

--- Ejercicios de Programación Funcional.py
+++ Ejercicios de Programación Funcional.py
@@ -25,7 +25,7 @@ def calc_total_basket():
 def calc_offer():
     total_with_discount = 0.0
     for element in basket:
-        basket_with_discount[element]= basket[element] / 1.21
+        basket_with_discount[element]= round(basket[element] / 1.21,2)
         total_with_discount = total_with_discount + basket[element] / 1.21
     return basket_with_discount,total_with_discount
     
@@ -33,7 +33,7 @@ def calc_offer():
 def calc_iva():
     total_with_iva = 0.0
     for element in basket:
-        basket_with_iva[element]= basket[element] / 1.21
+        basket_with_iva[element]= round(basket[element] / 1.21,2)
         total_with_iva = total_with_iva + basket[element] * 1.21
     return basket_with_iva,total_with_iva
 
@@ -46,13 +46,13 @@ print()
 
 basket_with_discount, total_with_discount = calc_offer()
 print('La cesta de la compra aplicado el descuento',basket_with_discount)
-print('El total de la cesta de la compra con descuento es: ',total_with_discount, '€')
+print('El total de la cesta de la compra con descuento es: ',round(total_with_discount,2), '€')
 
 print()
 
 basket_with_iva, total_with_iva = calc_iva()
 print('La cesta de la compra aplicado el IVA',basket_with_iva)
-print('El total de la cesta de la compra con IVA es: ',total_with_iva, '€')
+print('El total de la cesta de la compra con IVA es: ',round(total_with_iva,2), '€')
 
 """ OUTPUT
 La cesta de la compra original {'Memoria Ram': 75.0, 'Placa base': 200.0, 'Procesador': 350.0, 'Tarjeta grafica': 750.0}
